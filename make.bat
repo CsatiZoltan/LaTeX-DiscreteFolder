9e8ffc5e2c23f0ea45029023c0009cf456ebad8e
CsatiZoltan <csati_zoltan@freemail.hu>
2015-08-28 13:39:58 +0200

    Initial commit

---
+++ make.bat
@@ -0,0 +1,21 @@
+ECHO OFF
+
+:: Create a subdirectory containing the metafiles
+IF NOT exist %cd%\metafiles (mkdir metafiles)
+
+:: Obtain the command-line argument  (name of the .tex file)
+SET fileName=%1
+
+:: Run pdflatex
+pdflatex -output-directory=%cd% -aux-directory=metafiles %fileName%.tex
+
+:: Run bibtex
+bibtex --include-directory=metafiles metafiles\%fileName%.aux
+
+:: Create the nomenclature by invoking MakeIndex
+makeindex %fileName%.nlo -s %fileName%.ist -o %fileName%.nls
+
+:: Run pdflatex again to process the .nls file
+pdflatex -output-directory=%cd% -aux-directory=metafiles %fileName%.tex
+
+
